b85961f4a0e53790d6e44cea44e8ea37bb93e26e
antoniogisondi <anto.gisondi@tiscali.it>
2023-06-23 00:39:34 +0200

    Fixed bonus

--- src/components/AppPokemonList.vue
+++ src/components/AppPokemonList.vue
@@ -16,14 +16,14 @@ export default {
 </script>
 
 <template lang="">
-    <div class="container mt-4 bckg-white p-5 rounded-4">
-        <div class="row bckg-grey py-3 height overflow-auto" v-if="store.loading === false">
+    <div class="container mt-4 bckg-white p-5 rounded-4" v-if="store.loading === false">
+        <div class="row bckg-grey py-3 height overflow-auto">
             <div class="col-6 col-md-4 col-lg-3" v-for="(pokemon, index) in store.pokemon_list" :key="pokemon.id">
                 <AppPokemonCard :MyPokemon="pokemon"/>
             </div>
         </div>
-        <AppLoader v-else/>
     </div>
+    <AppLoader v-else/>
 </template>
 
 <style lang="scss" scoped>
