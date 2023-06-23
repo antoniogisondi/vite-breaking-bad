9262fb6079e660ab4ac6a593e0a110d0d63f4b13
antoniogisondi <anto.gisondi@tiscali.it>
2023-06-23 09:57:09 +0200

    Fixed

--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-    apiUrl: 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=15',
+    apiUrl: 'https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=10',
     pokemon_list: [],
     loading: true
 })
